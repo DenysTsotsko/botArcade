c72708f0722f7b25a826fc0281abdd3f96a16677
= <=>
2023-01-26 22:14:55 +0300

    add test data

---
+++ text.py
@@ -0,0 +1,60 @@
+HELP_COMMAND = """
+<b>/start</b> - <em>start the bot</em>
+<b>Balance 💰</b> - <em>there you can check your balance of points</em>
+<b>Games 🕹</b> - <em>there are all games</em>
+"""
+
+DICE_RULES = """
+ <b>Game 'Dice 🎲'</b>
+ 
+ <b>Rules:</b>
+ You have to roll dices and wait
+ until bot will make the turn, after that
+ you will see the result of game. Good luck! 🍀
+ 
+ <b>Buttons:</b>
+ 'Roll the 🎲' - <em>you make the turn.</em>
+ 'Stop game ⛔' - <em>stop and go into the game menu.</em>
+ 
+ <b>Price:</b>
+ roll  -2 points
+ win   +4 points
+ draw   0 points
+"""
+
+CUPS_START = """
+<b>Game "Magic cups ✨"</b>
+ 
+ <b>Rules:</b>
+ You have to mix cups, then choose one cup,
+  1, 2, or 3. Only one cup is lucky.
+  Then you will find out the result. Good luck🍀
+ 
+ <b>Buttons:</b>
+ 'Mix 🔄' - <em>you mix cups.</em>
+ 'Stop game ⛔' - <em>stop and go into the game menu.</em>
+ 
+ <b>Price:</b>
+ mix   -3 points
+ win   +12 points
+"""
+
+SLOT_START = """
+<b>Game 'Slots 🎰'</b>
+
+ <b>Rules:</b>
+ You have to spin slots. Only luck will help you.
+ 777 - the most biggest win. Try to spin. Good luck🍀
+
+ <b>Buttons:</b>
+ 'Spin ⭕' - <em>you spin slots.</em>
+ 'Stop game ⛔' - <em>stop and go into the game menu.</em>
+ 
+ <b>Price:</b>
+ spin   -2 points
+ 777    +100 points
+ 🍋🍋🍋 +50 points
+ 🫐🫐🫐 +25 points
+ bar    +10 points
+"""
+
